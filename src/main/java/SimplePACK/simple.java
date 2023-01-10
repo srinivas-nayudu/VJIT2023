10aa19f07f4a465cedd57bb00a64c132d5b3765a
srinivas-nayudu <122166174+srinivas-nayudu@users.noreply.github.com>
2023-01-10 14:47:11 +0530

    Add files via upload

---
+++ src/main/java/SimplePACK/simple.java
@@ -0,0 +1,16 @@
+package SimplePACK;
+
+import org.testng.annotations.Test;
+
+public class simple {
+  @Test
+  public void Test1()
+  {
+	  System.out.println("this is new script for comminting");
+	  
+  }
+  
+	
+	
+	
+}
